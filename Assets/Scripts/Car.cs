18bb6a7885f0a8f836651502ccb86a1bd914150e
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-14 14:03:32 -0700

    New track added, Animation Curve, engineTorque, maxSpeed limited

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
@@ -9,10 +9,18 @@ public class Car : MonoBehaviour
     [SerializeField] private float maxBrakeTorque;
     [SerializeField] private float maxSteerAngle;
 
-    [SerializeField] private CarChassis chassis;
+    
+
+    [SerializeField] private AnimationCurve engineTorqueCurve; // кривая крутящего момента
+    [SerializeField] private int maxSpeed;
+
+    private CarChassis chassis;
+
+    public float LinearVelocity => chassis.LinearVelocity;
 
     //DEBUG
-    public float ThrottleControl;
+    [SerializeField] private float linearVelocity;
+    public float ThrottleControl; // педаль газа
     public float SteerControl;
     public float BrakeControl;
     private void Start()
@@ -21,7 +29,14 @@ public class Car : MonoBehaviour
     }
     private void Update()
     {
-        chassis.MotorTorque = ThrottleControl * maxMotorTorque;
+        linearVelocity = LinearVelocity; // будет отсчитываться автоматически
+
+        //  крутящий момент
+        float engineTorque = engineTorqueCurve.Evaluate(LinearVelocity / maxSpeed) * maxMotorTorque; // изменится от 0 до 1
+
+        if (LinearVelocity >= maxSpeed) engineTorque = 0;
+
+        chassis.MotorTorque = ThrottleControl * engineTorque; 
         chassis.SteerAngle = SteerControl * maxSteerAngle;
         chassis.BrakeTorque = BrakeControl * maxBrakeTorque;       
     }
