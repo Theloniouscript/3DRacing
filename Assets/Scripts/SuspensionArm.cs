91244a0d6e245d27cbd432151397f1bf64e823db
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-16 07:00:57 -0700

    Suspension visual display

---
+++ Assets/Scripts/SuspensionArm.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspensionArm : MonoBehaviour
+{
+    [SerializeField] private Transform target; // как будет смещаться колесо
+    [SerializeField] private float factor; // коэффициент
+
+    private float baseOffset; // смещение
+
+    private void Start()
+    {
+        baseOffset = target.localPosition.y;
+    }
+
+    private void Update()
+    {
+        transform.localEulerAngles= new Vector3 (0, 0, (target.localPosition.y - baseOffset) * factor);
+    }
+}
