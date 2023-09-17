13e79543df9ef0b9012cb0d4ad815e0769c6ab46
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-17 04:48:05 -0700

    Camera Shaker at maximum speed

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    [SerializeField] private Car car;
+    [SerializeField] private float shakeAmount;
+    [SerializeField] [Range(0.0f, 1.0f)] private float normalizeSpeedShake;
+
+    private void Update()
+    {
+        if(car.NormalizeLinearVelocity >= normalizeSpeedShake) // при этом условии мы трясемся
+        {
+            transform.localPosition += Random.insideUnitSphere * shakeAmount * Time.deltaTime;
+        }
+    }
+}
