ac83a99676e2037d7f4f02e5c055f053c32d02fb
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-18 10:31:39 -0700

    TrackPoints[], TrackpointCircuit, Trackpoint prefab, test track

---
+++ Assets/Scripts/Track/TrackPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrackPoint : MonoBehaviour
+{
+    public TrackPoint Next;
+    public bool IsFirst;
+    public bool IsLast;
+
+    public event UnityAction<TrackPoint> Triggered; // Если автомобиль проехал до точки, вызов события
+
+    protected bool isTarget;
+    public bool IsTarget => isTarget;
+
+    public void Passed()
+    {
+        isTarget= false;
+    }
+
+    public void AssignAsTarget()
+    {
+        isTarget = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.GetComponent<Car>() == null) return;
+
+        Triggered?.Invoke(this);
+    }
+
+
+}
---
+++ Assets/Scripts/Track/TrackPointCircuit.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum TrackType
+{
+    Circular,
+    Sprint
+}
+
+public class TrackPointCircuit : MonoBehaviour
+{
+    public event UnityAction<int> LapCompleted;
+    public event UnityAction<TrackPoint> TrackPointTriggered;
+    [SerializeField] private TrackType type;
+    [SerializeField] private TrackPoint[] points;
+
+    private int lapsCompleted = -1;
+
+    private void Start()
+    {
+        LapCompleted += (t) => Debug.Log("Lap completed"); // Debug only
+        
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i].Triggered += OnTrackPointTriggered;
+        }
+
+        points[0].AssignAsTarget();
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i].Triggered -= OnTrackPointTriggered;
+        }
+    }
+
+    private void OnTrackPointTriggered(TrackPoint trackPoint)
+    {
+        if (trackPoint.IsTarget == false) return;
+
+        trackPoint.Passed();
+        trackPoint.Next?.AssignAsTarget();
+
+        TrackPointTriggered?.Invoke(trackPoint);
+
+        if (trackPoint.IsLast == true)
+        {
+            lapsCompleted++;
+
+            if(type == TrackType.Sprint)             
+                LapCompleted?.Invoke(lapsCompleted);
+            
+
+            if(type == TrackType.Circular)
+            {
+                if (lapsCompleted > 0)
+                    LapCompleted?.Invoke(lapsCompleted);
+            }
+        }
+    }
+}
