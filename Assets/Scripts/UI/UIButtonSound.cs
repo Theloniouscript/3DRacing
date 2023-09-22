d3ca1e96974d84ada015a8a386c795308980be2d
Lydia Pankratova <ecodeltaplan@gmail.com>
2023-09-22 04:20:56 -0700

    UI button audio added, audioassets import, audiomixer advanced

---
+++ Assets/Scripts/UI/UIButtonSound.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class UIButtonSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip hover;
+    [SerializeField] private AudioClip click;
+    private new AudioSource audio;
+
+    private UIButton[] uiButtons;
+
+    private void Start()
+    {
+        audio= GetComponent<AudioSource>();
+        uiButtons= GetComponentsInChildren<UIButton>(true); // мы должны пройтись по всем, даже неактивным игровым объектам
+
+        for(int i = 0; i < uiButtons.Length; i++)
+        {
+            uiButtons[i].PointerEnter += OnPointerEnter;
+            uiButtons[i].PointerClick += OnPointerClicked;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < uiButtons.Length; i++)
+        {
+            uiButtons[i].PointerEnter -= OnPointerEnter;
+            uiButtons[i].PointerClick -= OnPointerClicked;
+        }
+    }
+
+    private void OnPointerEnter(UIButton arg0)
+    {
+        audio.PlayOneShot(hover);
+    }
+
+    private void OnPointerClicked(UIButton arg0)
+    {
+        audio.PlayOneShot(click);
+    }
+}
